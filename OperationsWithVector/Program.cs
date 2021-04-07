e26fad3d89811f2a79254e9d9c8fd06fe01fbf1c
Александр Михайлов <alexander.mikhailov1991@outlook.com>
2021-04-07 18:05:45 +0700

    Add project files.

---
+++ OperationsWithVector/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OperationsWithVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector firstVector = new Vector(12, 5, 1);
+
+            Vector secondVector = new Vector(9, 3, 15);
+
+            var length = firstVector.LengthVector();
+
+            Console.WriteLine($"Vector LEngth = {length}");
+
+            var scalar = firstVector.ScalarVector(secondVector);
+
+            Console.WriteLine($"Dot product of vectors = {scalar}");
+
+            var production = firstVector.ProductionOfVectors(secondVector);
+
+            Console.WriteLine($"Vector product with another vector:\nY/Z: {production.X};\nZ/X: {production.Y};\nX/Y: {production.Z};");
+
+            var cosine = firstVector.CosineVector(secondVector);
+
+            Console.WriteLine($"Cosine of vectors = {cosine}");
+
+            var sum = firstVector.SumVector(secondVector);
+
+            Console.WriteLine($"Sum of Vectors:\nX: {sum.X};\nY: {sum.Y};\nZ: {sum.Z};");
+
+            var difference = firstVector.DifferenceVector(secondVector);
+
+            Console.WriteLine($"Difference of Vectors:\nX: {difference.X};\nY: {difference.Y};\nZ: {difference.Z};");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OperationsWithVector/Vector.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsWithVector
+{
+    public class Vector
+    {
+        public Vector(double x, double y, double z)
+        {
+            _x = x;
+
+            _y = y;
+
+            _z = z;
+        }
+
+        private readonly double _x = 0;
+        public double X
+        {
+            get => _x;
+        }
+
+        private readonly double _y = 0;
+        public double Y
+        {
+            get => _y;
+        }
+
+        private readonly double _z = 0;
+        public double Z
+        {
+            get => _z;
+        }
+
+        public double LengthVector()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+        }
+
+        public double ScalarVector(Vector vector)
+        {
+            return ((vector.X * X) + (vector.Y * Y) + (vector.Z * Z));
+        }
+
+        public Vector ProductionOfVectors(Vector vector)
+        {
+            double secondX = (vector.Y * Z - vector.Z * Y);
+
+            double secondY = (vector.Z * X - vector.X * Z);
+
+            double secondZ = (vector.X * Y - vector.Y * X);
+
+            Vector firstVector = new (secondX, secondY, secondZ);
+
+            return firstVector;
+        }
+
+        public double CosineVector(Vector vector)
+        {
+            double scalar = ScalarVector(vector);
+
+            double firstLength = LengthVector();
+
+            double secondLenght = vector.LengthVector();
+
+            double cosine = scalar / (firstLength * secondLenght);
+
+            return cosine;
+        }
+
+        public Vector SumVector(Vector vector)
+        {
+            Vector sum = new (vector.X + X, vector.Y + Y, vector.Z + Z);
+
+            return sum;
+        }
+
+        public Vector DifferenceVector(Vector vector)
+        {
+            Vector diff = new (X - vector.X, Y - vector.Y, Z - vector.Z);
+
+            return diff;
+        }
+    }
+}
+
